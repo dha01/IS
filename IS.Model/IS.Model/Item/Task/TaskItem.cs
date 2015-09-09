c215680e84a281aa71e4787a014d5f3fe571bd1a
dha <dha01@yandex.ru>
2015-09-09 20:48:23 +0500

    Добавлены страницы.
    Добавлены страницы OnlineText и SimpleTask.
    Добавлены страницы правил.

---
+++ IS.Model/IS.Model/Item/Task/TaskItem.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace IS.Model.Item.Task
+{
+	public class TaskItem
+	{
+		public int Id { get; set; }
+		public int TaskType { get; set; }
+		public int TaskNum { get; set; }
+		public string Header { get; set; }
+		public string Mem { get; set; }
+		public string Executor { get; set; }
+		public int Difficult { get; set; }
+		public bool Complite { get; set; }
+	}
+}
