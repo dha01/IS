28d0c4542200600fd214a442f384954101adeaf7
Kazhin Philipp <kazhin.philipp@gmail.com>
2015-09-15 09:31:50 +0500

    ПЕ-21б: Кажин Ф А: создал SQL запрос.

---
+++ SQL/Task_61_FA/test_query.sql
@@ -0,0 +1,13 @@
+create table Dbo.test_test
+(
+	test_test int identity(1,1) not null,
+	code nvarchar(128) not null,
+	mem nvarchar(max) null,
+)
+
+select dt.test
+from dbo.test dt
+	join dbo.test_two tt on tt.test_two = dt.test_test 
+where
+	dt.test > 0
+	and dt.test < 5
