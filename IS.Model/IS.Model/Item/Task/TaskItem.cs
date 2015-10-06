89df468838bd7e0e6636dcab1bd07753004f9056
dha <dha01@yandex.ru>
2015-10-06 12:09:36 +0500

    Уманов А.А.: создан репозиторий для задач
    Создан (изменен) класс для хранения данных по задаче.
    Создан интерфейс репозитория для задач.
    Создан базовый репозиторий для задач.

--- IS.Model/IS.Model/Item/Task/TaskItem.cs
+++ IS.Model/IS.Model/Item/Task/TaskItem.cs
@@ -1,19 +1,70 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 
 namespace IS.Model.Item.Task
 {
+	/// <summary>
+	/// Класс для хранения данных по задаче.
+	/// </summary>
 	public class TaskItem
 	{
+		/// <summary>
+		/// Идентификатор.
+		/// </summary>
 		public int Id { get; set; }
-		public int TaskType { get; set; }
-		public int TaskNum { get; set; }
+
+		/// <summary>
+		/// Префикс.
+		/// </summary>
+		public string TaskPrefix { get; set; }
+
+		/// <summary>
+		/// Номер.
+		/// </summary>
+		public int Number { get; set; }
+
+		/// <summary>
+		/// Заголовок.
+		/// </summary>
 		public string Header { get; set; }
+
+		/// <summary>
+		/// Описание.
+		/// </summary>
 		public string Mem { get; set; }
+
+		/// <summary>
+		/// Дата создания.
+		/// </summary>
+		public DateTime Created { get; set; }
+
+		/// <summary>
+		/// Срок исполнения.
+		/// </summary>
+		public DateTime Deadline { get; set; }
+
+		/// <summary>
+		/// Приоритет.
+		/// </summary>
+		public int Priority { get; set; }
+
+		/// <summary>
+		/// Исполнитель.
+		/// </summary>
 		public string Executor { get; set; }
-		public int Difficult { get; set; }
-		public bool Complite { get; set; }
+
+		/// <summary>
+		/// Автор.
+		/// </summary>
+		public string Author { get; set; }
+
+		/// <summary>
+		/// Признак того, что задача выполнена.
+		/// </summary>
+		public bool IsPerform { get; set; }
+
+		/// <summary>
+		/// Признак того, что задача открыта.
+		/// </summary>
+		public bool IsOpen { get; set; }
 	}
 }
