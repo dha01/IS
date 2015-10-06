df1032249f09ed0cc10fe9698c073aaed9743161
dha <dha01@yandex.ru>
2015-10-06 11:44:08 +0500

    Уманов А.А.: переименовано поле name в header

--- SQL/Demo_1/3. create table Task.task.sql
+++ SQL/Demo_1/3. create table Task.task.sql
@@ -4,7 +4,7 @@ create table Task.task
 	task int identity(1,1) not null,
 	number int not null,
 	task_prefix int not null,
-	name nvarchar(256) not null,
+	header nvarchar(256) not null,
 	mem nvarchar(max) not null,
 	created datetime not null constraint DF__task__created default (getdate()),
 	deadline datetime null,
