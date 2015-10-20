2cb1f85b187ec3e6078823b5f5d460409e587a19
Kazhin Philipp <kazhin.philipp@gmail.com>
2015-10-20 11:24:43 +0500

    ПЕ-21б Кажин Ф.А.: Добавление поля инкримента в таблицу Discipline.discipline

--- SQL/Task_23/create Discipline.discipline and create schema Discipline.sql
+++ SQL/Task_23/create Discipline.discipline and create schema Discipline.sql
@@ -1,10 +1,11 @@
 --Создание схемы Discipline
-create schema  Discipline 
+create schema  Discipline
 
 --Создание таблицы Discipline.discipline
 create table Discipline.discipline (
+	discipline int identity(1,1),
 	short_name varchar(30) NOT NULL,
 	full_name varchar(255) NOT NULL,
 	mem varchar(max),
-	primary key (short_name)
+	primary key (discipline)
 );
