b17be0a7d26ebbc8475aef88d1d923894ef96029
DenNikonov <den-nikonov@yandex.ru>
2015-10-26 23:46:12 +0500

    ПЕ-21б: Никонов Д.О.: Запрос на создание схемы и таблицы кафедр.
    Исправил запрос создания таблицы Cathedra.cathedra

--- SQL/Task_18/create Cathedra.cathedra.sql
+++ SQL/Task_18/create Cathedra.cathedra.sql
@@ -1,11 +1,13 @@
---Создание схемы "Cathedra"
+-- Создание схемы "Cathedra"
 create schema Cathedra
 
---Создание таблицы "cathedra"
-create table Cathedra.cathedra(
-	cathedra int identity (1,1),
-	full_name varchar(255),
-	short_name varchar(30),
+-- Создание таблицы "cathedra"
+create table Cathedra.cathedra
+(
+	cathedra int identity (1,1) not null,
+	full_name varchar(255) not null,
+	short_name varchar(30) not null,
 	faculty varchar(255),
-	primary key (cathedra)
+	constraint PK__cathedra primary key (cathedra),
+	constraint FK__cathedra__faculty foreign key (faculty) references Faculty.faculty
 )
