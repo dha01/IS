6bdb76393ab7026ea241a0ec0a5c84a1d4f172ab
OxanaPozdeeva <korombella@yandex.ru>
2015-10-27 21:36:49 +0500

    ПЕ-21б Поздеева О.В.: Создание таблицы и схемы Team.team
    Произведены изменения в соответствии с комментариями.
    Изменение кодировки.
    Изменение названия файла.
    Изменение типа поля дата.
    Изменение поля и ссылки на таблицу Specialty.specialty_detail.

---
+++ SQL/Task_14/create table Team.team.sql
@@ -0,0 +1,23 @@
+﻿create schema Team
+
+create table Team.team
+(
+	team int identity,
+	name varchar(50) not null,
+	data date,
+	specialty int,
+	constraint PK__team primary key (team),
+	constraint FK__team__specialty foreign key (specialty) references Specialty.specialty_detail
+)
+
+insert into Team.team
+		(name,
+		data)
+	values
+		(
+		'ПЕ-22б',
+		'2012-09-27'
+		)
+
+delete from Team.team
+	where team=11
