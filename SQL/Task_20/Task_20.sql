8201b1bf75cad23b8c836700d67ba58b1e3cb1a9
fluffry <fluffry@gmail.com>
2015-10-27 23:14:38 +0500

    ПЕ-21б Кетова С.О. заброс на составление таблицы Specialty.specialty_detail

---
+++ SQL/Task_20/Task_20.sql
@@ -0,0 +1,20 @@
+﻿-- Создание схемы "Specialty.specialty_detail"
+create schema Specialty
+
+-- Создание таблицы "Specialty.specialty_detail"
+create table Specialty.specialty
+(
+	Specialty_detail int identity (1,1) not null,
+	specialty int not null,
+	col_semestr int not null,
+	srock_obuch int not null,
+	qualification int not null,
+	form_of_study varchar(255) not null,
+	pay_space int not null,
+	lowcost_space int not null,
+
+	constraint PK__Specialty_detail primary key (Specialty_detail),
+	constraint FK__specialty__cathedra foreign key (specialty) references specialty.specialty,
+	constraint FK__specialty__cathedra foreign key (qualification) references qualification.qualification,
+	constraint FK__specialty__cathedra foreign key (form_of_study) references form_of_study.form_of_study,
+)
