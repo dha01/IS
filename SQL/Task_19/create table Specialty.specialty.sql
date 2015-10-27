af76f31d092670cff1575d747a48365d19d5fb67
BaTpyLLI <BaTpyLLI@gmail.com>
2015-10-27 23:15:21 +0500

    ПЕ-21б Шешуков А.С.: Запрос на создание табличной функции Specialty.specialty

--- SQL/Task_19/create table Specialty.specialty.sql
+++ SQL/Task_19/create table Specialty.specialty.sql
@@ -5,9 +5,9 @@ create schema Specialty
 create table Specialty.specialty
 (
 	Specialty int identity (1,1) not null,
-	Name varchar(255) not null,
+	full_Name varchar(255) not null,
 	short_name varchar(30) not null,
-	Kod_Specialty int not null,
+	Kod_Specialty varchar(30) not null,
 	cathedra int not null,
 	constraint PK__Specialty primary key (Specialty),
 	constraint FK__specialty__cathedra foreign key (cathedra) references Cathedra.cathedra
