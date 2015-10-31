5073ab9de9ae8cb49623556de57c94104c7ba7a7
OxanaPozdeeva <korombella@yandex.ru>
2015-10-31 16:57:22 +0500

    ПЕ-21б Поздеева О.В.: Создание таблицы и схемы Team.team
    Изменила название поля даты
    Убрала удаление записи из таблицы

--- SQL/Task_14/create table Team.team.sql
+++ SQL/Task_14/create table Team.team.sql
@@ -4,20 +4,17 @@ create table Team.team
 (
 	team int identity,
 	name varchar(50) not null,
-	data date,
-	specialty int,
+	create_date date,
+	specialty_detail int,
 	constraint PK__team primary key (team),
-	constraint FK__team__specialty foreign key (specialty) references Specialty.specialty_detail
+	constraint FK__team__specialty_detail foreign key (specialty_detail) references Specialty.specialty_detail
 )
 
 insert into Team.team
 		(name,
-		data)
+		create_date)
 	values
 		(
-		'ПЕ-22б',
-		'2012-09-27'
+		'ПЕ-21б',
+		'1999-06-12'
 		)
-
-delete from Team.team
-	where team=11
