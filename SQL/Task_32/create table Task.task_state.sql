c8aaa9902a8afec968bee2f59493e216f7f78cc8
EgorovKirill <d4bit@yandex.ru>
2015-11-03 09:08:03 +0500

    ПЕ-22б Егоров Кирилл запрос на создание таблицы Task.task_state.sql

---
+++ SQL/Task_32/create table Task.task_state.sql
@@ -0,0 +1,18 @@
+-- Создание таблицы-справочника Task.task-state
+create table Task.task_state (
+		task_state int identity,
+		name nvarchar(255) not null,
+		code varchar(30) not null,
+		mem varchar(max),
+		constraint PK__task_state primary key (discipline),
+);
+
+-- Добавление записи в таблицу Task.task_state
+insert into Task.task_state
+		(name,
+		code,
+		mem)
+values 
+		('В процессе выполнения', 'Progress', 'Задача находится в процессе выполнения'),
+		('Выполнена', 'Complete', 'Задача выполненена'),
+		('Ожидает проверки', 'Wait', 'Задача ожидает проверки')
