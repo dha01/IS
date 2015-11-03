39cf8a59a1597493d1ae0cd807d7b3c2b9f2c54c
danilz <danilzz@mail.ru>
2015-11-03 09:53:29 +0500

    ПЕ-22б Ожогов Н.: Запрос на создание таблицы Team.team

--- SQL/Task_33/create function Task.task_state_log.sql
+++
@@ -1,9 +0,0 @@
-﻿-- Создание таблицы "Task.task_state_log"
-create table Task.task_state_log 
-(
-	task int identity not null,
-	task_state int not null,
-	event_date datetime not null default getdate(),
-	constraint PK__task primary key (task),
-	constraint FK__task__task_state foreign key (task_state) references Task.task_state
-)
