45c1d7cff7286ca4a5b7af9dae998870bd85c599
MaxAntsiferov <i.antsiferoff@gmail.com>
2015-11-03 15:06:17 +0500

    ПЕ-21б Анциферов М.С.: Запрос на создание таблицы и схемы факультетов Исправил согласно комментариям.

--- SQL/task17.sql
+++ SQL/task17.sql
@@ -1,24 +1,21 @@
-﻿create schema Faculty
+﻿create schema Faculty 
+--Создание схемы Faculty
 
 create table Faculty.faculty
 (
 	faculty int identity(1,1),
-	full_Name varchar(255),
-	short_Name varchar(30),
+	full_name varchar(255),
+	short_name varchar(30),
 	primary key (faculty)
 )
 
-/*Добавление записи*/
-insert into Faculty.faculty
-           (full_Name
-           ,short_Name)
-     values
-           (
-			'мама мыла раму',
-			'рарара'
-		   )
-delete from Faculty.faculty
-	where faculty=1
+--Добавление записи
+insert into Faculty.faculty(full_name,short_name)
+values
+		(
+			'Информатика и вычислительная техника',
+			'ИВТ'
+		)
 
 
 
