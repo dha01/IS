5a6fe8fdb63ad3fe7abaae7ba2b1bbc1a0915cd3
danilz <danilzz@mail.ru>
2015-11-03 09:40:54 +0500

    ПЕ-22б Комарова Е.: Запрос ноздание таблицы Task.task_state_log

---
+++ SQL/Task_33/create function Task.task_state_log.sql
@@ -0,0 +1,9 @@
+﻿-- Создание таблицы "Task.task_state_log"
+create table Task.task_state_log
+(
+	task int identity not null,
+	task_state int not null,
+	event_date datetime not null default getdate(),
+	constraint PK__task primary key (task),
+	constraint FK__task__task_state foreign key (task_state) references Task.task_state
+)
