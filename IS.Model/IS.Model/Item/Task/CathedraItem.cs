31eb5dac3776df699c71b4704f8c287dbb07ca69
MaxAntsiferov <i.antsiferoff@gmail.com>
2015-11-05 09:50:28 +0500

    ПЕ-21 Анциферов М.С.: Создание класса для хранения сущности Cathedra Созданые свойства:Id,FullName,ShortName.

---
+++ IS.Model/IS.Model/Item/Task/CathedraItem.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+
+namespace IS.Model.Item.Task
+{
+	/// <summary>
+	/// Класс для хранения данных по задаче.
+	/// </summary>
+	class CathedraItem
+	{
+		/// <summary>
+		/// Идентификатор.
+		/// </summary>
+		public int Id { get; set; }
+
+		/// <summary>
+		/// Полное имя кафедры
+		/// </summary>
+		public string FullName { get; set; }
+
+		/// <summary>
+		/// Сокращенное имя кафедры
+		/// </summary>
+		public string ShortName { get; set; }
+
+
+	}
+}
