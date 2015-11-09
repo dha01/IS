5212ae2729c0c178335c8d34a7eeda1a0b9ed3c2
DenNikonov <den-nikonov@yandex.ru>
2015-11-09 23:42:03 +0500

    ПЕ-21б Никонов Д.О.: Создание репозитория дисциплин.
    Создал класс DisciplineItem для хранения данных по дисциплинам.
    Создал класс DisciplineRepository - интерфейс для работы с дисциплинами в БД.
    Создал класс IDisciplineRepository - интерфейс репозитория дисциплин.

---
+++ IS.Model/IS.Model/Item/Discipline/DisciplineItem.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace IS.Model.Item.Discipline
+{
+	/// <summary>
+	/// Класс для хранения данных по дисциплинам.
+	/// </summary>
+	public class DisciplineItem
+	{
+		/// <summary>
+		/// Идентификатор.
+		/// </summary>
+		public int Id { get; set; }
+
+		/// <summary>
+		/// Короткое название.
+		/// </summary>
+		public int Short_name { get; set; }
+
+		/// <summary>
+		/// Полное название.
+		/// </summary>
+		public string Full_name { get; set; }
+
+		/// <summary>
+		/// Описание.
+		/// </summary>
+		public string Mem { get; set; }
+	}
+}
