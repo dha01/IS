d409c6fba64bc74855fd5953568c29ba20bfd2a0
OxanaPozdeeva <korombella@yandex.ru>
2015-11-09 17:01:12 +0500

    ПЕ-21б Поздеева О.В.: Создание класса TeamItem
    Создала класс для хранения о сущности групп.

---
+++ IS.Model/IS.Model/Item/Team/TeamItem.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace IS.Model.Item.Team
+{
+	/// <summary>
+	/// Класс для хранения данных о сущности групп.
+	/// </summary>
+	public class TeamItem
+    {
+		/// <summary>
+		/// Идентификатор группы.
+		/// </summary>
+		public int Id { get; set; }
+
+		/// <summary>
+		/// Название группы.
+		/// </summary>
+		public string Name { get; set; }
+
+		/// <summary>
+		/// Дата создания группы.
+		/// </summary>
+		public DateTime? Create_date { get; set; }
+
+		/// <summary>
+		/// Идентификатор образовательной программы.
+		/// </summary>
+		public int Specialty_detail { get; set; }
+	}
+}
