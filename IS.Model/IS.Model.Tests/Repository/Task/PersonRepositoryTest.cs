f482187f89e4b747234f63bd0a2228b17d99dc3f
Philipp <kazhin.philipp@gmail.com>
2015-11-10 10:07:13 +0500

    ПЕ-21б Кажин Ф.А.: Репоситорий и тест Person.person

---
+++ IS.Model/IS.Model.Tests/Repository/Task/PersonRepositoryTest.cs
@@ -0,0 +1,183 @@
+﻿using System;
+using System.Transactions;
+using IS.Model.Item.Task;
+using IS.Model.Repository.Task;
+using NUnit.Framework;
+
+namespace IS.Model.Tests.Repository.Task
+{
+    /// <summary>
+    /// Тесты для репозитория людей.
+    /// </summary>
+    [Category("Integration")]
+    [TestFixture]
+    public class PersonRepositoryTest
+    {
+        #region Fields
+
+        /// <summary>
+        /// Транзакция.
+        /// </summary>
+        private TransactionScope _transactionScope;
+
+        /// <summary>
+        /// Репозиторий людей.
+        /// </summary>
+        private PersonRepository _personRepository;
+
+        private TaskItem _person; 
+        private TaskItem _personNew;
+
+        #endregion
+
+        #region SetUp
+
+        /// <summary>
+        /// Выполняется перед каждым тестом.
+        /// </summary>
+        [SetUp]
+        public void SetUp()
+        {
+            _transactionScope = new TransactionScope();
+            _personRepository = new PersonRepository();
+
+            _person = new TaskItem()
+            {
+                Author = "",
+                Deadline = DateTime.Now.AddDays(7).Date,
+                Created = DateTime.Now.Date,
+                Performer = "",
+                Header = "Тестирование людей",
+                IsOpen = true,
+                IsPerform = false,
+                Mem = "Описание",
+                Number = 1,
+                Priority = 0,
+                Prefix = TaskPrefix.Refactoring
+            };
+            _personNew = new TaskItem()
+            {
+                Author = "1",
+                Deadline = DateTime.Now.AddDays(8).Date,
+                Created = DateTime.Now.Date,
+                Performer = "2",
+                Header = "Тестирование людей 2",
+                IsOpen = false,
+                IsPerform = true,
+                Mem = "Описание2",
+                Number = 2,
+                Priority = 5,
+                Prefix = TaskPrefix.Demo
+            };
+        }
+
+        #endregion
+
+        #region TearDown
+
+        /// <summary>
+        /// Выполняется после каждого теста.
+        /// </summary>
+        [TearDown]
+        public void TearDown()
+        {
+            _transactionScope.Dispose();
+        }
+
+        #endregion
+
+        #region Methods
+
+        /// <summary>
+        /// Проверяет еквивалентны ли две задачи.
+        /// </summary>
+        /// <param name="first_task"></param>
+        /// <param name="second_task"></param>
+        private void AreEqualTasks(TaskItem first_task, TaskItem second_task)
+        {
+            Assert.AreEqual(first_task.Id, second_task.Id);
+            Assert.AreEqual(first_task.Author, second_task.Author);
+            Assert.AreEqual(first_task.Deadline, second_task.Deadline);
+            Assert.AreEqual(first_task.Performer, second_task.Performer);
+            Assert.AreEqual(first_task.Header, second_task.Header);
+            Assert.AreEqual(first_task.IsOpen, second_task.IsOpen);
+            Assert.AreEqual(first_task.IsPerform, second_task.IsPerform);
+            Assert.AreEqual(first_task.Mem, second_task.Mem);
+            Assert.AreEqual(first_task.Number, second_task.Number);
+            Assert.AreEqual(first_task.Priority, second_task.Priority);
+            Assert.AreEqual(first_task.Prefix, second_task.Prefix);
+        }
+
+        #endregion
+
+        #region Create
+
+        /// <summary>
+        /// Создает человека.
+        /// </summary>
+        [Test]
+        public void Create_Void_ReturnId()
+        {
+            _person.Id = _personRepository.Create(_person);
+            var result = _personRepository.Get(_person.Id);
+            AreEqualTasks(result, _person);
+        }
+
+        #endregion
+
+        #region Update
+
+        /// <summary>
+        /// Изменяет параметры человека.
+        /// </summary>
+        [Test]
+        public void Update_Void_ReturnChangedTask()
+        {
+            _person.Id = _personRepository.Create(_person);
+            var result = _personRepository.Get(_person.Id);
+            AreEqualTasks(result, _person);
+
+            _personNew.Id = _person.Id;
+            _personRepository.Update(_personNew);
+            result = _personRepository.Get(_person.Id);
+            AreEqualTasks(result, _personNew);
+
+        }
+
+        #endregion
+
+        #region Delete
+
+        /// <summary>
+        /// Удаляет человека.
+        /// </summary>
+        [Test]
+        public void Delete_Void_ReturnNull()
+        {
+            _person.Id = _personRepository.Create(_person);
+            var result = _personRepository.Get(_person.Id);
+            AreEqualTasks(result, _person);
+
+            _personRepository.Delete(_person.Id);
+            result = _personRepository.Get(_person.Id);
+            Assert.IsNull(result);
+        }
+
+        #endregion
+
+        #region GetList
+
+        /// <summary>
+        /// Получает список всех людей.
+        /// </summary>
+        [Test]
+        public void GetList_Void_ReturnNotEmptyListWithTask()
+        {
+            _person.Id = _personRepository.Create(_person);
+            var result = _personRepository.GetList().Find(x => x.Id == _person.Id);
+            AreEqualTasks(result, _person);
+        }
+
+        #endregion
+    }
+}
---
+++ IS.Model/IS.Model/Repository/Task/PersonRepository.cs
@@ -0,0 +1,115 @@
+﻿using System.Collections.Generic;
+using IS.Model.Helper;
+using IS.Model.Item.Task;
+
+namespace IS.Model.Repository.Task
+{
+    /// <summary>
+    /// Интерфейс репозитория людей.
+    /// </summary>
+    public class PersonRepository : ITaskRepository
+    {
+        /// <summary>
+        /// Получает человека по идентификатору.
+        /// </summary>
+        /// <param name="id">Идентификатор.</param>
+        /// <returns>Человека.</returns>
+        public TaskItem Get(int id)
+        {
+            using (var sqlh = new SqlHelper())
+            {
+                return sqlh.ExecMapping<TaskItem>(@"
+select
+	p.person Id,
+	p.last_name LastName,
+	p.first_name Name,
+	p.father_name Feather,
+    p.birthday Birthday
+from Person.Person p
+where p.person = @id", new { id });
+            }
+        }
+
+        /// <summary>
+        /// Обновляет данные у человека.
+        /// </summary>
+        /// <param name="task">Человека.</param>
+        public void Update(TaskItem person)
+        {
+            using (var sqlh = new SqlHelper())
+            {
+                sqlh.ExecNoQuery(@"
+update Person.person
+set
+	last_name = @LastName,
+	first_name = @Name,
+	father_name = @Feather,
+    birthday = @Birthday
+where task = @Id", person);
+            }
+        }
+
+        /// <summary>
+        /// Создает нового человека.
+        /// </summary>
+        /// <param name="task">Задача.</param>
+        /// <returns>Идентификатор созданного человека.</returns>
+        public int Create(TaskItem person)
+        {
+            using (var sqlh = new SqlHelper())
+            {
+                return sqlh.ExecScalar<int>(@"
+insert into Person.person
+(
+	last_name,
+	first_name,
+	father_name,
+    birthday
+)
+values
+(
+	@LastName,
+    @Name,
+    @Feather,
+    @Birthday
+)
+
+select scope_identity()", person);
+            }
+        }
+
+        /// <summary>
+        /// Удаляет человека.
+        /// </summary>
+        /// <param name="id">Идентификатор.</param>
+        public void Delete(int id)
+        {
+            using (SqlHelper sqlh = new SqlHelper())
+            {
+                sqlh.ExecNoQuery(@"
+delete from Person.person
+where person = @id", new { id });
+            }
+        }
+
+        /// <summary>
+        /// Получает список всех людей.
+        /// </summary>
+        /// <returns>Список людей.</returns>
+        public List<TaskItem> GetList()
+        {
+            using (var sqlh = new SqlHelper())
+            {
+                return sqlh.ExecMappingList<TaskItem>(@"
+select
+	p.person Id,
+	p.last_name LastName,
+	p.first_name Name,
+	p.father_name Feather,
+    p.birthday Birthday
+from Person.Person p
+	");
+            }
+        }
+    }
+}
