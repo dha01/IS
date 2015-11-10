c4c0ad414ad0377e74be9ceac08dfd9e39c73b63
Уманов А.А <dha01@yandex.ru>
2015-11-10 22:05:37 +0500

    Уманов А.А.: исправления
    Проект приведен в порядо. Тепрь он собирается без ошибок.
    Таймаут авторизации установлен на 6ч вместо 30 мин.
    Исправлен тест.
    Добавлены не включенные ранее в проект файлы.

--- IS.Model/IS.Model/Repository/Specialty/ISpecialtyRepository.cs
+++ IS.Model/IS.Model/Repository/Specialty/ISpecialtyRepository.cs
@@ -7,18 +7,37 @@ using IS.Model.Item.Specialty;
 
 namespace IS.Model.Repository.Specialty
 {
-    
-        /// <summary>
-        /// Интерфейс репозитория специальностей.
-        /// </summary>
-        public interface ISpecialtyRepository : IRepository<SpecialtyItem>
-        {
-            /// <summary>
-            /// Получает специальность по идентификатору.
-            /// </summary>
-            /// <param name="id">Идентификатор.</param>
-            /// <returns>Специальность.</returns>
-            SpecialtyItem Get(int id);
-        }
+	
+		/// <summary>
+		/// Интерфейс репозитория специальностей.
+		/// </summary>
+		public interface ISpecialtyRepository : IRepository<SpecialtyItem>
+		{
+			/// <summary>
+			/// Получает специальность по идентификатору.
+			/// </summary>
+			/// <param name="id">Идентификатор.</param>
+			/// <returns>Специальность.</returns>
+			SpecialtyItem Get(int id);
+
+			/// <summary>
+			/// Обновляет данные по специальности.
+			/// </summary>
+			/// <param name="specialty">Специальность.</param>
+			void Update(SpecialtyItem specialty);
+
+			/// <summary>
+			/// Создает новую специальность.
+			/// </summary>
+			/// <param name="specialty">Специальность.</param>
+			/// <returns>Идентификатор созданной специальности.</returns>
+			int Create(SpecialtyItem specialty);
+
+			/// <summary>
+			/// Удаляет специальность.
+			/// </summary>
+			/// <param name="id">Идентификатор.</param>
+			void Delete(int id);
+		}
 }
 
