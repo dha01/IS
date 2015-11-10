803094d737bec51197094db9000b54a42205125a
ksynya ko <xenia.oleg@ya.ru>
2015-11-10 10:40:21 +0500

    Колодкина К.О.:Исправлено

--- IS.Model/IS.Model/Repository/Specialty/SpecialtyRepository.cs
+++ IS.Model/IS.Model/Repository/Specialty/SpecialtyRepository.cs
@@ -7,30 +7,30 @@ using IS.Model.Item.Specialty;
 
 namespace IS.Model.Repository.Specialty
 {
-    /// <summary>
-    /// Интерфейс репозитория специальностей.
-    /// </summary>
-    public class SpecialtyRepository : ISpecialtyRepository
-    {
-        /// <summary>
-        /// Получает специальность по идентификатору.
-        /// </summary>
-        /// <param name="id">Идентификатор.</param>
-        /// <returns>Специальность.</returns>
-        public SpecialtyItem Get(int id)
-        {
-            using (var sqlh = new SqlHelper())
-            {
-                return sqlh.ExecMapping<SpecialtyItem>(@"
+	/// <summary>
+	/// Интерфейс репозитория специальностей.
+	/// </summary>
+	public class SpecialtyRepository : ISpecialtyRepository
+	{
+		/// <summary>
+		/// Получает специальность по идентификатору.
+		/// </summary>
+		/// <param name="id">Идентификатор.</param>
+		/// <returns>Специальность.</returns>
+		public SpecialtyItem Get(int id)
+		{
+			using (var sqlh = new SqlHelper())
+			{
+				return sqlh.ExecMapping<SpecialtyItem>(@"
 select
 	s.specialty Id,
 	s.full_name FullName,
 	s.short_name ShortName,
 	s.code Code
 from Specialty.specialty s
-where t.specialty = @id", new { id });
-            }
-        }
-    } 
+where s.specialty = @id", new { id });
+			}
+		}
+	} 
 }
 
