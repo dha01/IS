21d0074af06ae65fa618b3ee34145c64d21c9146
ksynya ko <xenia.oleg@ya.ru>
2015-11-10 10:19:35 +0500

    Колодкина К.О.:создание класса - репозитория, интерфейса для работы с данными по специальностям в БД

---
+++ IS.Model/IS.Model/Repository/Specialty/SpecialtyRepository.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using IS.Model.Helper;
+using IS.Model.Item.Specialty;
+
+namespace IS.Model.Repository.Specialty
+{
+    /// <summary>
+    /// Интерфейс репозитория специальностей.
+    /// </summary>
+    public class SpecialtyRepository : ISpecialtyRepository
+    {
+        /// <summary>
+        /// Получает специальность по идентификатору.
+        /// </summary>
+        /// <param name="id">Идентификатор.</param>
+        /// <returns>Специальность.</returns>
+        public SpecialtyItem Get(int id)
+        {
+            using (var sqlh = new SqlHelper())
+            {
+                return sqlh.ExecMapping<SpecialtyItem>(@"
+select
+	s.specialty Id,
+	s.full_name FullName,
+	s.short_name ShortName,
+	s.code Code
+from Specialty.specialty s
+where t.specialty = @id", new { id });
+            }
+        }
+    } 
+}
+
