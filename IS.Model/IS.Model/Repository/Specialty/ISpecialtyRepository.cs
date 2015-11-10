c399df6f4b06ca81c3d5dd52f1984239744bdd76
Уманов А.А <dha01@yandex.ru>
2015-11-10 22:07:53 +0500

    Уманов А.А.: табуляция

--- IS.Model/IS.Model/Repository/Specialty/ISpecialtyRepository.cs
+++ IS.Model/IS.Model/Repository/Specialty/ISpecialtyRepository.cs
@@ -1,43 +1,36 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using IS.Model.Item.Access;
-using IS.Model.Item.Specialty;
+﻿using IS.Model.Item.Specialty;
 
 namespace IS.Model.Repository.Specialty
 {
-	
+	/// <summary>
+	/// Интерфейс репозитория специальностей.
+	/// </summary>
+	public interface ISpecialtyRepository : IRepository<SpecialtyItem>
+	{
 		/// <summary>
-		/// Интерфейс репозитория специальностей.
+		/// Получает специальность по идентификатору.
 		/// </summary>
-		public interface ISpecialtyRepository : IRepository<SpecialtyItem>
-		{
-			/// <summary>
-			/// Получает специальность по идентификатору.
-			/// </summary>
-			/// <param name="id">Идентификатор.</param>
-			/// <returns>Специальность.</returns>
-			SpecialtyItem Get(int id);
+		/// <param name="id">Идентификатор.</param>
+		/// <returns>Специальность.</returns>
+		SpecialtyItem Get(int id);
 
-			/// <summary>
-			/// Обновляет данные по специальности.
-			/// </summary>
-			/// <param name="specialty">Специальность.</param>
-			void Update(SpecialtyItem specialty);
+		/// <summary>
+		/// Обновляет данные по специальности.
+		/// </summary>
+		/// <param name="specialty">Специальность.</param>
+		void Update(SpecialtyItem specialty);
 
-			/// <summary>
-			/// Создает новую специальность.
-			/// </summary>
-			/// <param name="specialty">Специальность.</param>
-			/// <returns>Идентификатор созданной специальности.</returns>
-			int Create(SpecialtyItem specialty);
+		/// <summary>
+		/// Создает новую специальность.
+		/// </summary>
+		/// <param name="specialty">Специальность.</param>
+		/// <returns>Идентификатор созданной специальности.</returns>
+		int Create(SpecialtyItem specialty);
 
-			/// <summary>
-			/// Удаляет специальность.
-			/// </summary>
-			/// <param name="id">Идентификатор.</param>
-			void Delete(int id);
-		}
+		/// <summary>
+		/// Удаляет специальность.
+		/// </summary>
+		/// <param name="id">Идентификатор.</param>
+		void Delete(int id);
+	}
 }
-
