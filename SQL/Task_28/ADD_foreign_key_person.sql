03d88ddebdaf6de9a3c8a9db72b718ef37560874
Dannn45 <dannn45@gmail.com>
2015-11-12 10:09:33 +0600

    ПЕ-21б Фокеев добавление поля person

---
+++ SQL/Task_28/ADD_foreign_key_person.sql
@@ -0,0 +1,7 @@
+﻿--Добавить поле person для создания вторичного ключа
+
+alter table Access.[user]
+add
+	person int identity(1,1)
+	constraint FK_Person foreign key (person)
+	references Person.person (person)
