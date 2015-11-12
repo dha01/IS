d8e11e265b9862305836a5867308b2c3f996ed2d
danilz <danilzz@mail.ru>
2015-11-12 09:57:27 +0500

    ПЕ-22б Ожогов Николай: Создание класса AuditoryItem

---
+++ IS.Model/IS.Model/Item/Auditory/AuditoryItem.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace IS.Model.Item.Auditory
+{
+    /// <summary>
+    /// Класс для хранения данных по аудиториям
+    /// </summary>
+    public class AuditoryItem
+    {
+        /// <summary>
+        /// Номер
+        /// </summary>
+        public int Number { get; set; }
+
+        /// <summary>
+        /// Полное наименование
+        /// </summary>
+        public string FullName { get; set; }
+
+        /// <summary>
+        /// Описание
+        /// </summary>
+        public string Memo { get; set; }
+
+        /// <summary>
+        /// Этаж
+        /// </summary>
+        public int Level { get; set; }
+
+        /// <summary>
+        /// Вместимость
+        /// </summary>
+        public int Capacity { get; set; }
+    }
+}
