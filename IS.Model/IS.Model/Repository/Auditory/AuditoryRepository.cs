faa15482ac6cc961c837c4b01fd9d1d96ddd3005
danilz <danilzz@mail.ru>
2015-11-12 11:06:14 +0500

    ПЕ-22б Журавлев Д.А.: Изменение табуляции.

--- IS.Model/IS.Model/Repository/Auditory/AuditoryRepository.cs
+++ IS.Model/IS.Model/Repository/Auditory/AuditoryRepository.cs
@@ -7,21 +7,21 @@ using IS.Model.Helper;
 
 namespace IS.Model.Repository.Auditory
 {
-    /// <summary>
-    /// Класс репозитория аудиторий.
-    /// </summary>
-    public class AuditoryRepository : IAuditoryRepository
-    {
-        /// <summary>
-        /// Получает аудиторию по идентификатору.
-        /// </summary>
-        /// <param name="id">Идентификатор.</param>
-        /// <returns>Аудитория.</returns>
-        public AuditoryItem Get(int id)
-        {
-            using (var sqlh = new SqlHelper())
-            {
-                return sqlh.ExecMapping<AuditoryItem>(@"
+	/// <summary>
+	/// Класс репозитория аудиторий.
+	/// </summary>
+	public class AuditoryRepository : IAuditoryRepository
+	{
+		/// <summary>
+		/// Получает аудиторию по идентификатору.
+		/// </summary>
+		/// <param name="id">Идентификатор.</param>
+		/// <returns>Аудитория.</returns>
+		public AuditoryItem Get(int id)
+		{
+			using (var sqlh = new SqlHelper())
+			{
+				return sqlh.ExecMapping<AuditoryItem>(@"
 select
 	a.auditory Id,
 	a.number Number,
@@ -31,44 +31,44 @@ select
 	a.capacity Capacity
 from Auditory.auditory a
 where a.auditory = @id", new { id });
-            }
-        }
+			}
+		}
 
-        /// <summary>
-        /// Обновляет данные по аудитории.
-        /// </summary>
-        /// <param name="auditory">Аудитория.</param>
-        public void Update(AuditoryItem auditory)
-        {
+		/// <summary>
+		/// Обновляет данные по аудитории.
+		/// </summary>
+		/// <param name="auditory">Аудитория.</param>
+		public void Update(AuditoryItem auditory)
+		{
 
-        }
+		}
 
-        /// <summary>
-        /// Создает новую аудиторию.
-        /// </summary>
-        /// <param name="auditory">Аудитория.</param>
-        /// <returns>Идентификатор созданной аудитории.</returns>
-        public int Create(AuditoryItem auditory)
-        {
-            return 0;
-        }
+		/// <summary>
+		/// Создает новую аудиторию.
+		/// </summary>
+		/// <param name="auditory">Аудитория.</param>
+		/// <returns>Идентификатор созданной аудитории.</returns>
+		public int Create(AuditoryItem auditory)
+		{
+			return 0;
+		}
 
-        /// <summary>
-        /// Удаляет аудиторию.
-        /// </summary>
-        /// <param name="id">Идентификатор.</param>
-        public void Delete(int id)
-        {
+		/// <summary>
+		/// Удаляет аудиторию.
+		/// </summary>
+		/// <param name="id">Идентификатор.</param>
+		public void Delete(int id)
+		{
 
-        }
+		}
 
-        /// <summary>
-        /// Получает список всех аудиторий.
-        /// </summary>
-        /// <returns>Список аудиторий.</returns>
-        public List<AuditoryItem> GetList()
-        {
-            return null;
-        }
-    }
+		/// <summary>
+		/// Получает список всех аудиторий.
+		/// </summary>
+		/// <returns>Список аудиторий.</returns>
+		public List<AuditoryItem> GetList()
+		{
+			return null;
+		}
+	}
 }
--- IS.Model/IS.Model/Repository/Auditory/IAuditoryRepository.cs
+++ IS.Model/IS.Model/Repository/Auditory/IAuditoryRepository.cs
@@ -6,41 +6,41 @@ using IS.Model.Item.Auditory;
 
 namespace IS.Model.Repository.Auditory
 {
-    /// <summary>
-    /// Интерфейс репозитория аудиторий.
-    /// </summary>
-    public interface IAuditoryRepository : IRepository<AuditoryItem>
-    {
-        /// <summary>
-        /// Получает аудиторию по идентификатору.
-        /// </summary>
-        /// <param name="id">Идентификатор.</param>
-        /// <returns>Аудитория.</returns>
-        AuditoryItem Get(int id);
+	/// <summary>
+	/// Интерфейс репозитория аудиторий.
+	/// </summary>
+	public interface IAuditoryRepository : IRepository<AuditoryItem>
+	{
+		/// <summary>
+		/// Получает аудиторию по идентификатору.
+		/// </summary>
+		/// <param name="id">Идентификатор.</param>
+		/// <returns>Аудитория.</returns>
+		AuditoryItem Get(int id);
 
-        /// <summary>
-        /// Обновляет данные по аудитории.
-        /// </summary>
-        /// <param name="auditory">Аудитория.</param>
-        void Update(AuditoryItem auditory);
+		/// <summary>
+		/// Обновляет данные по аудитории.
+		/// </summary>
+		/// <param name="auditory">Аудитория.</param>
+		void Update(AuditoryItem auditory);
 
-        /// <summary>
-        /// Создает новую аудиторию.
-        /// </summary>
-        /// <param name="auditory">Аудитория.</param>
-        /// <returns>Идентификатор созданной аудитории.</returns>
-        int Create(AuditoryItem auditory);
+		/// <summary>
+		/// Создает новую аудиторию.
+		/// </summary>
+		/// <param name="auditory">Аудитория.</param>
+		/// <returns>Идентификатор созданной аудитории.</returns>
+		int Create(AuditoryItem auditory);
 
-        /// <summary>
-        /// Удаляет аудиторию.
-        /// </summary>
-        /// <param name="id">Идентификатор.</param>
-        void Delete(int id);
+		/// <summary>
+		/// Удаляет аудиторию.
+		/// </summary>
+		/// <param name="id">Идентификатор.</param>
+		void Delete(int id);
 
-        /// <summary>
-        /// Получает список всех аудиторий.
-        /// </summary>
-        /// <returns>Список аудиторий.</returns>
-        List<AuditoryItem> GetList();
-    }
+		/// <summary>
+		/// Получает список всех аудиторий.
+		/// </summary>
+		/// <returns>Список аудиторий.</returns>
+		List<AuditoryItem> GetList();
+	}
 }
