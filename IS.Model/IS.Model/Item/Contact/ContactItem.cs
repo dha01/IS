e6ab491aa6501439886ebf47547e2c747526867d
WereWolf <WereWolf0016@gmail.com>
2015-11-25 10:25:09 +0500

    ПЕ-22б Наумочкин Ю.А. Task_84 Создать класс ContactRepository

---
+++ IS.Model/IS.Model/Item/Contact/ContactItem.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace IS.Model.Item.Contact
+{
+    /// <summary>
+    /// Класс для хранения данных по контакту.
+    /// </summary>
+    public class ContactItem
+    {
+        /// <summary>
+        /// Идентификатор.
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Идентификатор типа контакта.
+        /// </summary>
+        public ContactType Type { get; set; }
+
+        /// <summary>
+        /// Значение.
+        /// </summary>
+        public string Value { get; set; }
+    }
+    /// <summary>
+    /// Тип контакта.
+    /// </summary>
+    public enum ContactType
+    {
+        MobilePhone,	/*Мобильный телефон*/
+        CityPhone,		/*Городской телефон*/
+        Skype,		    /*Скайп*/
+        Facebook,		/*Фейсбук*/
+        VK,			    /*ВКонтакте*/
+        Steam,		    /*Стим*/
+        Twitter,		/*Твиттер*/
+        Instagram		/*Инстаграм*/
+    }
+}
