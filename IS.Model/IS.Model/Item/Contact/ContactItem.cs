e314845c8d1918123e3f6c4c781b13b016cb2b28
WereWolf <WereWolf0016@gmail.com>
2015-11-25 12:26:25 +0500

    ПЕ-22б Наумочкин Ю.А. Task_84 Дополнение и исправления

--- IS.Model/IS.Model/Item/Contact/ContactItem.cs
+++ IS.Model/IS.Model/Item/Contact/ContactItem.cs
@@ -5,38 +5,70 @@ using System.Text;
 
 namespace IS.Model.Item.Contact
 {
-    /// <summary>
-    /// Класс для хранения данных по контакту.
-    /// </summary>
-    public class ContactItem
-    {
-        /// <summary>
-        /// Идентификатор.
-        /// </summary>
-        public int Id { get; set; }
+	/// <summary>
+	/// Класс для хранения данных по контакту.
+	/// </summary>
+	public class ContactItem
+	{
+		/// <summary>
+		/// Идентификатор.
+		/// </summary>
+		public int Id { get; set; }
 
-        /// <summary>
-        /// Идентификатор типа контакта.
-        /// </summary>
-        public ContactType Type { get; set; }
+		/// <summary>
+		/// Идентификатор типа контакта.
+		/// </summary>
+		public ContactType Type { get; set; }
 
-        /// <summary>
-        /// Значение.
-        /// </summary>
-        public string Value { get; set; }
-    }
-    /// <summary>
-    /// Тип контакта.
-    /// </summary>
-    public enum ContactType
-    {
-        MobilePhone,	/*Мобильный телефон*/
-        CityPhone,		/*Городской телефон*/
-        Skype,		    /*Скайп*/
-        Facebook,		/*Фейсбук*/
-        VK,			    /*ВКонтакте*/
-        Steam,		    /*Стим*/
-        Twitter,		/*Твиттер*/
-        Instagram		/*Инстаграм*/
-    }
+		/// <summary>
+		/// Значение.
+		/// </summary>
+		public string Value { get; set; }
+	}
+	
+	/// <summary>
+	/// Тип контакта.
+	/// </summary>
+	public enum ContactType
+	{
+		/// <summary>
+		/// Мобильный телефон.
+		/// </summary>
+		MobilePhone,
+
+		/// <summary>
+		/// Городской телефон/
+		/// </summary>
+		CityPhone,
+
+		/// <summary>
+		/// Скайп/
+		/// </summary>
+		Skype,
+
+		/// <summary>
+		/// Фейсбук.
+		/// </summary>
+		Facebook,
+
+		/// <summary>
+		/// ВКонтакте.
+		/// </summary>
+		VK,
+
+		/// <summary>
+		/// Стим.
+		/// </summary>
+		Steam,
+
+		/// <summary>
+		/// Твиттер.
+		/// </summary>
+		Twitter,
+
+		/// <summary>
+		/// Инстаграм.
+		/// </summary>
+		Instagram
+	}
 }
