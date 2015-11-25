d2897a2ba8eff52944a2d2eecb9dd6e2ebbd42d0
Philipp <kazhin.philipp@gmail.com>
2015-11-25 22:53:05 +0500

    ПЕ-21б Кажин Ф.А.: Создание запроса на добавление роли в Access.role. Исправление ошибок в коде.

---
+++ SQL/Task_77/insert into Access.role.sql
@@ -0,0 +1,10 @@
+﻿insert into Person.person
+	(last_name
+	,first_name
+	,father_name
+	,birthday)
+	values
+	('Кажин','Филипп','Александрович',null),
+	('Черных','Сергей','Генадьевич',null),
+	('Турицына','Татьяна','Игоревна',null)
+
