729116d0eb96c500afc0b6e66a4eeed629ace261
DenNikonov <den-nikonov@yandex.ru>
2015-11-26 10:06:19 +0500

    ПЕ-21б Никонов Д.О. Создать интерфейс для работы с дисциплинами
    Добавлены представления и контроллер в проект
    Добавлена проверка на доступ
    Изменен внешний вид и скрипт на удаление

--- IS.Mvc/IS.Mvc/Controllers/DisciplineController.cs
+++ IS.Mvc/IS.Mvc/Controllers/DisciplineController.cs
@@ -9,106 +9,108 @@ using IS.Mvc.Models;
 
 namespace IS.Mvc.Controllers
 {
-    /// <summary>
-    /// Контролер для работы с данными по дисциплинам.
-    /// </summary>
-    public class DisciplineController : Controller
-    {
-        private DisciplineService _disciplineService;
+	/// <summary>
+	/// Контролер для работы с данными по дисциплинам.
+	/// </summary>
+	public class DisciplineController : Controller
+	{
+		private DisciplineService _disciplineService;
 
-        public DisciplineController()
-        {
-            _disciplineService = new DisciplineService();
-        }
+		public DisciplineController()
+		{
+			_disciplineService = new DisciplineService();
+		}
 
-        /// <summary>
-        /// Страница с дисциплиной. Если идентификатор не указан перенаправляет на список дисциплин.
-        /// </summary>
-        /// <param name="id">Идентификатор дисциплины.</param>
-        /// <returns></returns>
-        public ActionResult Index(int? id)
-        {
-            if (id.HasValue)
-            {
-                return View("Index", _disciplineService.GetById(id.Value));
-            }
-            else
-            {
-                return RedirectToAction("List");
-            }
-        }
+		/// <summary>
+		/// Страница с дисциплиной. Если идентификатор не указан перенаправляет на список дисциплин.
+		/// </summary>
+		/// <param name="id">Идентификатор дисциплины.</param>
+		/// <returns></returns>
+		public ActionResult Index(int? id)
+		{
+			Access.CheckAccess("Discipline.Reader");
+			if (id.HasValue)
+			{
+				return View("Index", _disciplineService.GetById(id.Value));
+			}
+			else
+			{
+				return RedirectToAction("List");
+			}
+		}
 
-        /// <summary>
-        /// Список дисциплин.
-        /// </summary>
-        /// <returns></returns>
-        public ActionResult List()
-        {
-            return View("List", _disciplineService.GetList().OrderBy(x => x.ShortName).ToList());
-        }
+		/// <summary>
+		/// Список дисциплин.
+		/// </summary>
+		/// <returns></returns>
+		public ActionResult List()
+		{
+			Access.CheckAccess("Discipline.Reader");
+			return View("List", _disciplineService.GetList().OrderBy(x => x.ShortName).ToList());
+		}
 
-        /// <summary>
-        /// Создает новую дисциплину.
-        /// </summary>
-        /// <returns></returns>
-        [ValidateInput(false)]
-        public ActionResult Create(DisciplineItem discipline)
-        {
-          //  Access.CheckAccess("Discipline.Creator");
-            return RedirectToAction("List", new { id = _disciplineService.Create(discipline) });
-        }
+		/// <summary>
+		/// Создает новую дисциплину.
+		/// </summary>
+		/// <returns></returns>
+		[ValidateInput(false)]
+		public ActionResult Create(DisciplineItem discipline)
+		{
+			Access.CheckAccess("Discipline.Creator");
+			return RedirectToAction("List", new { id = _disciplineService.Create(discipline) });
+		}
 
-        /// <summary>
-        /// Интерфейс для создания задачи.
-        /// </summary>
-        /// <returns></returns>
-        public ActionResult New()
-        {
-           // Access.CheckAccess("Discipline.Creator");
-            var defaultitem = new DisciplineItem()
-            {
-                ShortName = "Краткое ",
-                FullName = "Полное ",
-                Mem = "Описание"
-            };
-            return View(defaultitem);
-        }
+		/// <summary>
+		/// Интерфейс для создания задачи.
+		/// </summary>
+		/// <returns></returns>
+		public ActionResult New()
+		{
+			Access.CheckAccess("Discipline.Creator");
+			var defaultitem = new DisciplineItem()
+			{
+				ShortName = "Краткое ",
+				FullName = "Полное ",
+				Mem = "Описание"
+			};
+			return View(defaultitem);
+		}
 
-        /// <summary>
-        /// Сохраняет изменения в дисциплине.
-        /// </summary>
-        /// <returns></returns>
-        [ValidateInput(false)]
-        public ActionResult Update(DisciplineItem discipline)
-        {
-          //  Access.CheckAccess("Discipline.Updater");
-            _disciplineService.Update(discipline);
-            return RedirectToAction("Index", new { id = discipline.Id });
-            
-        }
+		/// <summary>
+		/// Сохраняет изменения в дисциплине.
+		/// </summary>
+		/// <returns></returns>
+		[ValidateInput(false)]
+		public ActionResult Update(DisciplineItem discipline)
+		{
+			Access.CheckAccess("Discipline.Updater");
+			_disciplineService.Update(discipline);
+			return RedirectToAction("Index", new { id = discipline.Id });
+			
+		}
 
 
-        /// <summary>
-        /// Интерфейс для редактирования дисциплины.
-        /// </summary>
-        /// <returns></returns>
-        public ActionResult Edit(int id)
-        {
-          //  Access.CheckAccess("Discipline.Updater");
-            return View( _disciplineService.GetById(id));
+		/// <summary>
+		/// Интерфейс для редактирования дисциплины.
+		/// </summary>
+		/// <returns></returns>
+		public ActionResult Edit(int id)
+		{
+			Access.CheckAccess("Discipline.Updater");
+			return View( _disciplineService.GetById(id));
 
-        }
+		}
 
-        /// <summary>
-        /// Интерфейс для редактирования задачи.
-        /// </summary>
-        /// <returns></returns>
-        public ActionResult Delete(int id)
-        {
-            // Access.CheckAccess("Discipline.Deleter");
-            _disciplineService.Delete(id);
-            return RedirectToAction("Index");
-        }
+		/// <summary>
+		/// Интерфейс для редактирования задачи.
+		/// </summary>
+		/// <returns></returns>
+		public ActionResult Delete(int id)
+		{
+			 Access.CheckAccess("Discipline.Deleter");
+			_disciplineService.Delete(id);
+			return RedirectToAction("Index");
+		}
 
-    }
+	}
 }
