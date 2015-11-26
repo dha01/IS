6fe6a0b65dc7320a4b19c5e9927d4e3d2ec3cf5e
Sysoenko N.A <stels_unit111@mail.ru>
2015-11-26 12:04:04 +0500

    Сысоенко Н.А.: Создание репозитория преподователей и метода get
    воооот

--- SQL/Task_34/create function Lecturer.GetLecturerByDate.sql
+++ SQL/Task_34/create function Lecturer.GetLecturerByDate.sql
@@ -1,5 +1,6 @@
 ﻿-- Создание табличной функции "Lecturer.GetLecturerByDate"
-create function Lecturer.GetLecturerByDate
+/*
+create function Person.GetLecturerByDate
 ( @date datetime )
 returns table
 as
@@ -11,4 +12,13 @@ from Person.lecturer l
 where l.event_date <= @date
 group by l.person, l.cathedra
 having sum(l.act) = 1
+*/
 
+select
+	p.person Id,
+	p.last_name LastName,
+	p.first_name FirstName,
+	p.father_name FatherName,
+	p.birthday Birthday
+from Person.Person p
+join Person.GetLecturerByDate(GETDATE()) on p.person = Person.GetLecturerByDate.person
