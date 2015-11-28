14d3911f55989d52c225b18f41de29fe1e05e80a
AniLetuM <olym29082@yandex.ru>
2015-11-28 19:32:01 +0500

    ПЕ-22б Шерстнёв Создал репозиторий учебного курса
    И внёс пару правок в SQL таблицу

---
+++ IS.Model/IS.Model.Tests/Repository/Specialty/SpecialtyDetailRepositoryTests.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Transactions;
+using IS.Model.Item.SpecialtyDetail;
+using IS.Model.Repository.SpecialtyDetail;
+using NUnit.Framework;
+
+namespace IS.Model.Tests.Repository.SpecialtyDetail
+{
+	/// <summary>
+	/// Тесты для репозитория учбных курсов.
+	/// </summary>
+	[Category("Integration")]
+	[TestFixture]
+	public class SpecialtyDetailRepositoryTests
+	{
+		#region Fields
+
+		/// <summary>
+		/// Транзакция.
+		/// </summary>
+		private TransactionScope _transactionScope;
+
+		/// <summary>
+		/// Репозиторий курсов.
+		/// </summary>
+		private SpecialtyDetailRepository _specialty_detailRepository;
+
+		private SpecialtyDetailItem _specialty_detail;
+		private SpecialtyDetailItem _specialty_detailNew;
+
+		#endregion
+
+		#region SetUp
+
+		/// <summary>
+		/// Выполняется перед каждым тестом.
+		/// </summary>
+		[SetUp]
+		public void SetUp()
+		{
+			_transactionScope = new TransactionScope();
+			_specialty_detailRepository = new SpecialtyDetailRepository();
+
+			_specialty_detail = new SpecialtyDetailItem()
+			{
+				ActualDate = DateTime.Now.Date,
+				SemestrCount = 4,
+				TrainingPeriod = 1,
+				PaySpace = 128,
+				LowcostSpace = 45,
+			};
+			_specialty_detailNew = new SpecialtyDetailItem()
+			{
+				ActualDate = DateTime.Now.Date,
+				SemestrCount = 3,
+				TrainingPeriod = 2,
+				PaySpace = 64,
+				LowcostSpace = 34,
+			};
+		}
+
+		#endregion
+
+		#region TearDown
+
+		/// <summary>
+		/// Выполняется после каждого теста.
+		/// </summary>
+		[TearDown]
+		public void TearDown()
+		{
+			_transactionScope.Dispose();
+		}
+
+		#endregion
+
+		#region Methods
+
+		/// <summary>
+		/// Проверяет эквивалентны ли два курса.
+		/// </summary>
+		/// <param name="first_specialty_detail"></param>
+		/// <param name="second_specialty_detail"></param>
+		private void AreEqualSpecialtyDetails(SpecialtyDetailItem first_specialty_detail, SpecialtyDetailItem second_specialty_detail)
+		{
+			Assert.AreEqual(first_specialty_detail.Id, second_specialty_detail.Id);
+			Assert.AreEqual(first_specialty_detail.ActualDate, second_specialty_detail.ActualDate);
+			Assert.AreEqual(first_specialty_detail.SemestrCount, second_specialty_detail.SemestrCount);
+			Assert.AreEqual(first_specialty_detail.TrainingPeriod, second_specialty_detail.TrainingPeriod);
+			Assert.AreEqual(first_specialty_detail.PaySpace, second_specialty_detail.PaySpace);
+			Assert.AreEqual(first_specialty_detail.LowcostSpace, second_specialty_detail.LowcostSpace);
+		}
+
+		#endregion
+
+		#region Create
+
+		/// <summary>
+		/// Создает учебный курс.
+		/// </summary>
+		[Test]
+		public void Create_Void_ReturnId()
+		{
+			_specialty_detail.Id = _specialty_detailRepository.Create(_specialty_detail);
+			var result = _specialty_detailRepository.Get(_specialty_detail.Id);
+			AreEqualSpecialtyDetails(result, _specialty_detail);
+		}
+
+		#endregion
+
+		#region Update
+
+		/// <summary>
+		/// Изменяет параметры учебного курса.
+		/// </summary>
+		[Test]
+		public void Update_Void_ReturnChangedSpecialtyDetail()
+		{
+			_specialty_detail.Id = _specialty_detailRepository.Create(_specialty_detail);
+			var result = _specialty_detailRepository.Get(_specialty_detail.Id);
+			AreEqualSpecialtyDetails(result, _specialty_detail);
+
+			_specialty_detailNew.Id = _specialty_detail.Id;
+			_specialty_detailRepository.Update(_specialty_detailNew);
+			result = _specialty_detailRepository.Get(_specialty_detail.Id);
+			AreEqualSpecialtyDetails(result, _specialty_detailNew);
+
+		}
+
+		#endregion
+
+		#region Delete
+
+		/// <summary>
+		/// Удаляет курс.
+		/// </summary>
+		[Test]
+		public void Delete_Void_ReturnNull()
+		{
+			_specialty_detail.Id = _specialty_detailRepository.Create(_specialty_detail);
+			var result = _specialty_detailRepository.Get(_specialty_detail.Id);
+			AreEqualSpecialtyDetails(result, _specialty_detail);
+
+			_specialty_detailRepository.Delete(_specialty_detail.Id);
+			result = _specialty_detailRepository.Get(_specialty_detail.Id);
+			Assert.IsNull(result);
+		}
+
+		#endregion
+
+		#region GetList
+
+		/// <summary>
+		/// Получает список всех контактов.
+		/// </summary>
+		[Test]
+		public void GetList_Void_ReturnNotEmptyListWithSpecialtyDetails()
+		{
+			_specialty_detail.Id = _specialty_detailRepository.Create(_specialty_detail);
+			var result = _specialty_detailRepository.GetList().Find(x => x.Id == _specialty_detail.Id);
+			AreEqualSpecialtyDetails(result, _specialty_detail);
+		}
+
+		#endregion
+	}
+}
---
+++ IS.Model/IS.Model/Repository/Specialty/SpecialtyDetailRepository.cs
@@ -0,0 +1,136 @@
+﻿using IS.Model.Helper;
+using IS.Model.Item.SpecialtyDetail;
+
+namespace IS.Model.Repository.SpecialtyDetail
+{
+	/// <summary>
+	/// Интерфейс репозитория для работы с учебными курсами.
+	/// </summary>
+	public class SpecialtyDetailRepository : ISpecialtyDetailRepository
+	{
+		/// <summary>
+		/// Получает учебный курс по идентификатору.
+		/// </summary>
+		/// <param name="id">Идентификатор.</param>
+		/// <returns>Учебный курс.</returns>
+		public SpecialtyDetailItem Get(int id)
+		{
+			using (var sqlh = new SqlHelper())
+			{
+				return sqlh.ExecMapping<SpecialtyDetailItem>(@"
+select
+	sd.specialty_detail Id,
+	sd.actual_date ActualDate,
+	sd.semestr_count SemestrCount,
+	sd.training_period TrainingPeriod,
+	sd.pay_space PaySpace,
+	sd.lowcost_space LowcostSpace
+	q.qualification Qualification,
+	st.form_study FormStudy,
+from Specialty.specialty_detail sd
+	join Specialty.qualification q on q.qualification = sd.qualification
+	join Specialty.form_study st on st.form_study = sd.form_study
+where sd.specialty_detail = @id", new { id });
+			}
+		}
+
+		/// <summary>
+		/// Обновляет данные по учебному курсу.
+		/// </summary>
+		/// <param name="specialty_detail">Учебный курс.</param>
+		public void Update(SpecialtyDetailItem specialty)
+		{
+			using (var sqlh = new SqlHelper())
+			{
+				sqlh.ExecNoQuery(@"
+update Specialty.specialty_detail
+set
+	specialty_detail = @Id,
+	actual_date = @ActualDate,
+	semestr_count = @SemestrCount,
+	training_period = @TrainingPeriod,
+	pay_space = @PaySpace,
+	lowcost_space = @LowcostSpace,
+	qualification = (select top 1 q.qualification from Specialty.qualification q where q.qualification = @Qualification),
+	form_study = (select top 1 st.form_study from Specialty.form_study st where st.form_study = @FormStudy),
+where specialty_detail = @Id", specialty_detail);
+			}
+		}
+
+		/// <summary>
+		/// Создает новую учебный курс.
+		/// </summary>
+		/// <param name="specialty_detail">Учеьный курс.</param>
+		/// <returns>Идентификатор созданного учебного курса.</returns>
+		public int Create(SpecialtyDetailItem specialty)
+		{
+			using (var sqlh = new SqlHelper())
+			{
+				return sqlh.ExecScalar<int>(@"
+insert into Specialty.specialty_detail
+(
+	specialty_detail,
+	actual_date,
+	semestr_count,
+	training_period,
+	pay_space,
+	lowcost_space,
+	qualification,
+	form_study,
+)
+values
+(
+	@Id,
+	@ActualDate,
+	@SemestrCount,
+	@TrainingPeriod,
+	@PaySpace,
+	@LowcostSpace,
+	(select top 1 q.qualification from Specialty.qualification q where q.qualification = @Qualification),
+	(select top 1 st.form_study from Specialty.form_study st where st.form_study = @FormStudy),
+)
+
+select scope_identity()", specialty_detail);
+			}
+		}
+
+		/// <summary>
+		/// Удаляет учебный курс.
+		/// </summary>
+		/// <param name="id">Идентификатор.</param>
+		public void Delete(int id)
+		{
+			using (SqlHelper sqlh = new SqlHelper())
+			{
+				sqlh.ExecNoQuery(@"
+delete from Specialty.specialty_detail
+where specialty_detail = @id", new { id });
+			}
+		}
+
+		/// <summary>
+		/// Получает список всех учебных курсов.
+		/// </summary>
+		/// <returns>Список курсов.</returns>
+		public List<SpecialtyDetailItem> GetList()
+		{
+			using (var sqlh = new SqlHelper())
+			{
+				return sqlh.ExecMappingList<SpecialtyDetailItem>(@"
+select
+	sd.specialty_detail Id,
+	sd.actual_date ActualDate,
+	sd.semestr_count SemestrCount,
+	sd.training_period TrainingPeriod,
+	sd.pay_space PaySpace,
+	sd.lowcost_space LowcostSpace
+	q.qualification Qualification,
+	st.form_study FormStudy,
+from Specialty.specialty_detail sd
+	join Specialty.qualification q on q.qualification = sd.qualification
+	join Specialty.form_study st on st.form_study = sd.form_study");
+			}
+		}
+	} 
+}
+
