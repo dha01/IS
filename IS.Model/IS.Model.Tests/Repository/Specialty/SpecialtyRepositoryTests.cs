fe17d21e18da6a728d9cb31f015f2703c4a88c9b
test <test@mail.ru>
2015-12-01 09:20:37 +0500

    ПЕ-22б Егоров Виталий: Создание метода Update в классе SpecialtyRepository

--- IS.Model/IS.Model.Tests/Repository/Specialty/SpecialtyRepositoryTests.cs
+++ IS.Model/IS.Model.Tests/Repository/Specialty/SpecialtyRepositoryTests.cs
@@ -10,109 +10,106 @@ using NUnit.Framework;
 
 namespace IS.Model.Tests.Repository.Specialty
 {
-    /// <summary>
-    /// Тесты для репозитория специальностей.
-    /// </summary>
-    [Category("Integration")]
-    [TestFixture]
-    public class SpecialtyRepositoryTests
-    {
-        #region Fields
+	/// <summary>
+	/// Тесты для репозитория специальностей.
+	/// </summary>
+	[Category("Integration")]
+	[TestFixture]
+	public class SpecialtyRepositoryTests
+	{
+		#region Fields
 
-        /// <summary>
-        /// Транзакция.
-        /// </summary>
-        private TransactionScope _transactionScope;
+		/// <summary>
+		/// Транзакция.
+		/// </summary>
+		private TransactionScope _transactionScope;
 
-        /// <summary>
-        /// Репозиторий специальностей.
-        /// </summary>
-        private SpecialtyRepository _specialtyRepository;
+		/// <summary>
+		/// Репозиторий специальностей.
+		/// </summary>
+		private SpecialtyRepository _specialtyRepository;
 
-        private SpecialtyItem _specialty;
-        private SpecialtyItem _specialtyNew;
+		private SpecialtyItem _specialty;
+		private SpecialtyItem _specialtyNew;
 
-        #endregion
+		#endregion
 
-        #region SetUp
+		#region SetUp
 
-        /// <summary>
-        /// Выполняется перед каждым тестом.
-        /// </summary>
-        [SetUp]
-        public void SetUp()
-        {
-            _transactionScope = new TransactionScope();
-            _specialtyRepository = new SpecialtyRepository();
+		/// <summary>
+		/// Выполняется перед каждым тестом.
+		/// </summary>
+		[SetUp]
+		public void SetUp()
+		{
+			_transactionScope = new TransactionScope();
+			_specialtyRepository = new SpecialtyRepository();
 
-            _specialty = new SpecialtyItem()
-            {
-                FullName = "Программное обеспечение вычислительной техники и автоматизированных систем",
-                ShortName = "Ифн",
-                Code = "230105",
-                Cathedra = 45655454
-            };
-            _specialtyNew = new SpecialtyItem()
-            {
-                FullName = "Сисадмин",
-                ShortName = "Сис",
-                Code = "123456",
-                Cathedra = 87654321
-            };
-        }
+			_specialty = new SpecialtyItem()
+			{
+				FullName = "Программное обеспечение вычислительной техники и автоматизированных систем",
+				ShortName = "Ифн",
+				Code = "230105"
+			};
+			_specialtyNew = new SpecialtyItem()
+			{
+				FullName = "Сисадмин",
+				ShortName = "Сис",
+				Code = "123456"
+			};
+		}
 
-        #endregion
+		#endregion
 
-        #region TearDown
+		#region TearDown
 
-        /// <summary>
-        /// Выполняется после каждого теста.
-        /// </summary>
-        [TearDown]
-        public void TearDown()
-        {
-            _transactionScope.Dispose();
-        }
+		/// <summary>
+		/// Выполняется после каждого теста.
+		/// </summary>
+		[TearDown]
+		public void TearDown()
+		{
+			_transactionScope.Dispose();
+		}
 
-        #endregion
+		#endregion
 
-        #region Methods
+		#region Methods
 
-        /// <summary>
-        /// Проверяет эквивалентны ли две специальности.
-        /// </summary>
-        /// <param name="first_specialty"></param>
-        /// <param name="second_specialty"></param>
-        private void AreEqualSpecialtys(SpecialtyItem first_specialty, SpecialtyItem second_specialty)
-        {
-            Assert.AreEqual(first_specialty.Id, second_specialty.Id);
-            Assert.AreEqual(first_specialty.FullName, second_specialty.FullName);
-            Assert.AreEqual(first_specialty.ShortName, second_specialty.ShortName);
-            Assert.AreEqual(first_specialty.Code, second_specialty.Code);
-            Assert.AreEqual(first_specialty.Cathedra, second_specialty.Cathedra);
-        }
-                
-        #endregion
+		/// <summary>
+		/// Проверяет эквивалентны ли две специальности.
+		/// </summary>
+		/// <param name="first_specialty">Первая специальность.</param>
+		/// <param name="second_specialty">Вторая специальность.</param> 
+		private void AreEqualSpecialtys(SpecialtyItem first_specialty, SpecialtyItem second_specialty)
+		{
+			Assert.AreEqual(first_specialty.Id, second_specialty.Id);
+			Assert.AreEqual(first_specialty.FullName, second_specialty.FullName);
+			Assert.AreEqual(first_specialty.ShortName, second_specialty.ShortName);
+			Assert.AreEqual(first_specialty.Code, second_specialty.Code);
+		}
+				
+		#endregion
 
-        #region Update
+		#region Update
 
-        /// <summary>
-        /// Изменяет параметры специальности.
-        /// </summary>
-        [Test]
-        public void Update_Void_ReturnChangedSpecialty()
-        {
-            _specialty.Id = _specialtyRepository.Create(_specialty);
-            var result = _specialtyRepository.Get(_specialty.Id);
-            AreEqualSpecialtys(result, _specialty);
+		/// <summary>
+		/// Изменяет параметры специальности.
+		/// </summary>
+		[Test]
+		public void Update_Void_ReturnChangedSpecialty()
+		{
+			_specialty.Id = _specialtyRepository.Create(_specialty);
+			var result = _specialtyRepository.Get(_specialty.Id);
+			AreEqualSpecialtys(result, _specialty);
 
-            _specialtyNew.Id = _specialty.Id;
-            _specialtyRepository.Update(_specialtyNew);
-            result = _specialtyRepository.Get(_specialty.Id);
-            AreEqualSpecialtys(result, _specialtyNew);
+			_specialtyNew.Id = _specialty.Id;
+			_specialtyRepository.Update(_specialtyNew);
+			result = _specialtyRepository.Get(_specialty.Id);
+			AreEqualSpecialtys(result, _specialtyNew);
 
-        }
+		}
 
-        #endregion
-    }
+		#endregion
+	}
 }
--- IS.Model/IS.Model/Item/Specialty/SpecialtyItem.cs
+++ IS.Model/IS.Model/Item/Specialty/SpecialtyItem.cs
@@ -2,29 +2,29 @@
 
 namespace IS.Model.Item.Specialty
 {
-    /// <summary>
-    /// Класс для хранения данных о сущности cпециальности.
-    /// </summary>
-    public class SpecialtyItem
-    {
-        /// <summary>
-        /// Идентификатор специальности.
-        /// </summary>
-        public int Id { get; set; }
+	/// <summary>
+	/// Класс для хранения данных о сущности cпециальности.
+	/// </summary>
+	public class SpecialtyItem
+	{
+		/// <summary>
+		/// Идентификатор специальности.
+		/// </summary>
+		public int Id { get; set; }
 
-        /// <summary>
-        /// Полное название специальности.
-        /// </summary>
-        public string FullName { get; set; }
+		/// <summary>
+		/// Полное название специальности.
+		/// </summary>
+		public string FullName { get; set; }
 
-        /// <summary>
-        /// Сокращенное название специальности.
-        /// </summary>
-        public string ShortName { get; set; }
+		/// <summary>
+		/// Сокращенное название специальности.
+		/// </summary>
+		public string ShortName { get; set; }
 
-        /// <summary>
-        /// Код специальности.
-        /// </summary>
-        public char Code { get; set; }
-    }
+		/// <summary>
+		/// Код специальности.
+		/// </summary>
+		public string Code { get; set; }
+	}
 }
