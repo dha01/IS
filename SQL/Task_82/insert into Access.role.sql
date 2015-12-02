2457dcbbe284a64af88f48156ccbbc3a459f34be
tasik73 <tasik73@yandex.ru>
2015-12-02 23:57:25 +0500

    ПЕ-21б Турицына Т.И.: создала интерфейс для работы с факультетами

---
+++ SQL/Task_82/insert into Access.role.sql
@@ -0,0 +1,8 @@
+﻿insert into Access.role
+	(code
+	,mem)
+	values
+	('Faculty.Creator','Позволяет создавать запись о факультете'),
+	('Faculty.Updater','Позволяет редактировать запись о факультете'),
+	('Faculty.Deleter','Позволяет удалять запись о факультете'),
+	('Faculty.Reader','Позволяет открывать запись о факультете')
