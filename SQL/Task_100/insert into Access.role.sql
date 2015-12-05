3e96b995ede2d13f576ebfb815c0c8688c3bccb7
magnum088 <magnum088@gmail.com>
2015-12-05 19:16:32 +0500

    ПЕ-22б Ожогов Н.: Создать интерфейс для работы с группами.

---
+++ SQL/Task_100/insert into Access.role.sql
@@ -0,0 +1,8 @@
+﻿insert into Access.role
+	(code
+	,mem)
+	values
+	('Role.Creator','Позволяет создавать группы'),
+	('Role.Updater','Позволяет редактировать группы'),
+	('Role.Deleter','Позволяет удалять группы'),
+	('Role.Reader','Позволяет посмотреть группы')
