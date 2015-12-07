c3ec548eeb82253f21cd3c269e1936c70447c9f9
magnum088 <magnum088@gmail.com>
2015-12-07 22:01:12 +0500

    ПЕ-22б Ожогов Н.: Создать интерфейс для работы с группами.

--- IS.Model/IS.Model/Item/Team/TeamItem.cs
+++ IS.Model/IS.Model/Item/Team/TeamItem.cs
@@ -2,24 +2,29 @@
 
 namespace IS.Model.Item.Team
 {
-    /// <summary>
-    /// Класс для хранения данных о сущности групп.
-    /// </summary>
-    public class TeamItem
-    {
-        /// <summary>
-        /// Идентификатор группы.
-        /// </summary>
-        public int Id { get; set; }
+	/// <summary>
+	/// Класс для хранения данных о сущности групп.
+	/// </summary>
+	public class TeamItem
+	{
+		/// <summary>
+		/// Идентификатор группы.
+		/// </summary>
+		public int Id { get; set; }
 
-        /// <summary>
-        /// Название группы.
-        /// </summary>
-        public string Name { get; set; }
+		/// <summary>
+		/// Название группы.
+		/// </summary>
+		public string Name { get; set; }
 
-        /// <summary>
-        /// Дата создания группы.
-        /// </summary>
-        public DateTime? CreateDate { get; set; }
-    }
+		/// <summary>
+		/// Дата создания группы.
+		/// </summary>
+		public DateTime? CreateDate { get; set; }
+
+		/// <summary>
+		/// Идентификатор специальности.
+		/// </summary>
+		public int SpecialtyDetailId { get; set; }
+	}
 }
