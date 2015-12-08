f8e9de972d590ca6d0fa4eb8816de3683138ec3e
WereWolf0016 <WereWolf0016@gmail.com>
2015-12-08 09:53:10 +0500

    Create repository for task

---
+++ IS.Model/IS.Model.Tests/Service/AuditoryServiceTests.cs
@@ -0,0 +1,199 @@
+﻿using System;
+using System.Collections.Generic;
+using IS.Model.Item.Auditory;
+using IS.Model.Repository.Auditory;
+using IS.Model.Service;
+using Moq;
+using NUnit.Framework;
+
+namespace IS.Model.Tests.Service
+{
+	/// <summary>
+	/// Тесты для сервиса аудиторий.
+	/// </summary>
+	[Category("Unit")]
+	[TestFixture]
+	public class AuditoryServiceTests
+	{
+		#region Fields
+
+		/// <summary>
+		/// Сервис аудиторий.
+		/// </summary>
+		private AuditoryService _auditoryService;
+
+		/// <summary>
+		/// Репозиторий аудиторий.
+		/// </summary>
+		private IAuditoryRepository _auditoryRepository;
+
+		/// <summary>
+		/// Тестовая аудитория.
+		/// </summary>
+		private AuditoryItem _auditory;
+
+		#endregion
+
+		#region SetUp
+
+		/// <summary>
+		/// Выполняется перед каждым тестом.
+		/// </summary>
+		[SetUp]
+		public void SetUp()
+		{
+			_auditoryRepository = Mock.Of<IAuditoryRepository>();
+			_auditoryService = new AuditoryService(_auditoryRepository);
+
+			_auditory = new AuditoryItem()
+			{
+				/*Id = 1,
+				Author = "",
+				Deadline = DateTime.Now.AddDays(7).Date,
+				Created = DateTime.Now.Date,
+				Performer = "",
+				Header = "Тестирование демонстрационной аудиторийи",
+				IsOpen = true,
+				IsPerform = false,
+				Mem = "Описание",
+				Number = 1,
+				Priority = 0,
+				Prefix = AuditoryPrefix.Refactoring*/
+			}; 
+		}
+
+		#endregion
+
+		#region AuditoryService
+
+		/// <summary>
+		/// Конструктор без параметров.
+		/// </summary>
+		[Test]
+		public void AuditoryService_Void_Success()
+		{
+			var s = new AuditoryService();
+		}
+
+		#endregion
+
+		#region Create
+
+		/// <summary>
+		/// Создает аудиторийу.
+		/// </summary>
+		[Test]
+		public void Create_Void_ReturnId()
+		{
+			var list = new List<AuditoryItem>(){ _auditory };
+			
+			Mock.Get(_auditoryRepository).Setup(x => x.Create(_auditory)).Returns(_auditory.Id);
+			Mock.Get(_auditoryRepository).Setup(x => x.GetList()).Returns(list);
+
+			var result = _auditoryService.Create(_auditory);
+			Assert.AreEqual(result, _auditory.Id);
+		}
+
+/*		/// <summary>
+		/// Создает аудиторию с пустым заголовком.
+		/// </summary>
+		[ExpectedException(ExpectedMessage = "Поле 'Header' не должно быть пустым.")]
+		[Test]
+		public void Create_EmptyHeader_ReturnException()
+		{
+			_auditory.Header = null;
+			_auditoryService.Create(_auditory);
+		}
+
+		/// <summary>
+		/// Создает аудиторийу с пустым описанием.
+		/// </summary>
+		[ExpectedException(ExpectedMessage = "Поле 'Mem' не должно быть пустым.")]
+		[Test]
+		public void Create_EmptyMem_ReturnException()
+		{
+			_auditory.Mem = null;
+			_auditoryService.Create(_auditory);
+		}
+*/
+		#endregion
+
+		#region Update
+
+		/// <summary>
+		/// Изменяет данные о аудитории.
+		/// </summary>
+		[Test]
+		public void Update_Void_Success()
+		{
+			Mock.Get(_auditoryRepository).Setup(x => x.Get(_auditory.Id)).Returns(_auditory);
+			_auditoryService.Update(_auditory);
+		}
+
+/*		/// <summary>
+		/// Изменяет заголовок на пустой.
+		/// </summary>
+		[ExpectedException(ExpectedMessage = "Поле 'Header' не должно быть пустым.")]
+		[Test]
+		public void Update_EmptyHeader_ReturnException()
+		{
+			_auditory.Header = null;
+			_auditoryService.Update(_auditory);
+		}
+
+		/// <summary>
+		/// Изменяет описание на пустое.
+		/// </summary>
+		[ExpectedException(ExpectedMessage = "Поле 'Mem' не должно быть пустым.")]
+		[Test]
+		public void Update_EmptyMem_ReturnException()
+		{
+			_auditory.Mem = null;
+			_auditoryService.Update(_auditory);
+		}
+
+		/// <summary>
+		/// Изменяет описание на пустое.
+		/// </summary>
+		[ExpectedException(ExpectedMessage = "аудиторийа не найдена.")]
+		[Test]
+		public void Update_AuditoryNotExists_ReturnException()
+		{
+			Mock.Get(_auditoryRepository).Setup(x => x.Get(_auditory.Id)).Returns((AuditoryItem)null);
+			_auditoryService.Update(_auditory);
+		}
+*/
+		#endregion
+
+		#region Delete
+
+		/// <summary>
+		/// Удаляет аудиторию.
+		/// </summary>
+		[Test]
+		public void Delete_Void_Success()
+		{
+			_auditoryService.Delete(_auditory.Id);
+		}
+
+		#endregion
+
+		#region GetList
+
+		/// <summary>
+		/// Получает список аудиторий.
+		/// </summary>
+		[Test]
+		public void GetList_Void_ReturnAuditoryList()
+		{
+			var list = new List<AuditoryItem> { _auditory };
+
+			Mock.Get(_auditoryRepository).Setup(x => x.GetList()).Returns(list);
+			var result = _auditoryService.GetList();
+
+			Assert.AreEqual(result.Count, list.Count);
+		}
+
+		#endregion
+	}
+}
---
+++ IS.Model/IS.Model/Service/AuditoryService.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using IS.Model.Item.Auditory;
+using IS.Model.Repository.Auditory;
+
+namespace IS.Model.Service
+{
+	/// <summary>
+	/// Сервис для работы с аудиториями.
+	/// </summary>
+	public class AuditoryService
+	{
+		#region Fields
+		
+		/// <summary>
+		/// Репозиторий задач.
+		/// </summary>
+		private IAuditoryRepository _auditoryRepository;
+
+		#endregion
+
+		#region Constructors
+
+		/// <summary>
+		/// Конструктор без параметров.
+		/// </summary>
+		public AuditoryService()
+		{
+			_auditoryRepository = new AuditoryRepository();
+		}
+
+		/// <summary>
+		/// Конструктор класс.
+		/// </summary>
+		/// <param name="auditory_repository">Интерфейс репозитория аудиторий.</param>
+		public AuditoryService(IAuditoryRepository auditory_repository)
+		{
+			_auditoryRepository = auditory_repository;
+		}
+
+		#endregion
+
+		#region Methods
+
+		/// <summary>
+		/// Получает аудиторию по идентификатору.
+		/// </summary>
+		/// <param name="id">Идентификатор.</param>
+		/// <returns>Аудитория.</returns>
+		public AuditoryItem GetById(int id)
+		{
+			return _auditoryRepository.Get(id);
+		}
+
+		/// <summary>
+		/// Создает аудиторию.
+		/// </summary>
+		/// <param name="auditory">Аудитория.</param>
+		/// <returns>Идентификаторо созданной задачи.</returns>
+		public int Create(AuditoryItem auditory)
+		{/*a.auditory Id,
+	a.number Number,
+	a.full_name FullName,
+	a.memo Memo,
+	a.level Level,
+	a.capacity Capacity*/
+		}
+
+		/// <summary>
+		/// Измененяет данные об аудитории.
+		/// </summary>
+		/// <param name="auditory">Аудитория.</param>
+		public void Update(AuditoryItem auditory)
+		{
+
+		}
+
+		/// <summary>
+		/// Удаляет аудиторию.
+		/// </summary>
+		/// <param name="id">Идентификатор.</param>
+		public void Delete(int id)
+		{
+			_auditoryRepository.Delete(id);
+		}
+
+
+		/// <summary>
+		/// Получает список аудиторий.
+		/// </summary>
+		public List<AuditoryItem> GetList()
+		{
+			return _auditoryRepository.GetList();
+		}
+
+
+		#endregion
+	}
+}
