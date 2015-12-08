d4448defc0de68090ee29f718638439e2d4a38df
Томашов М.В <maxim.tomashov93@gmail.com>
2015-12-09 00:24:01 +1200

    ПЕ-22б Яковлев Я.О. Исправил ошибку(поставил запятую).

--- SQL/Task_41/create table TrainingPlan.lesson2auditory.sql
+++ SQL/Task_41/create table TrainingPlan.lesson2auditory.sql
@@ -1,12 +1,12 @@
--- Создание схемы TrainingPlan
+п»ї-- РЎРѕР·РґР°РЅРёРµ СЃС…РµРјС‹ TrainingPlan
 create schema TrainingPlan
 go
 
--- Создание таблицы TrainingPlan.lesson
+-- РЎРѕР·РґР°РЅРёРµ С‚Р°Р±Р»РёС†С‹ TrainingPlan.lesson
 create table TrainingPlan.lesson2auditory(
 	lesson2auditory int identity,
 	lesson int not null,
-	auditory int not null  
+	auditory int not null,  
 	constraint PK__lesson primary key (lesson),
 	constraint FK__lesson2auditory__auditory foreign key (auditory) references Auditory.auditory
 )	
