3c16b0f334779bc6631d02f2c700c0c569ea15a4
boync <sempai206@gmail.com>
2015-12-09 01:15:17 +0500

    Исправил ошибки от 08.12

--- SQL/Task_38/create table Calendar.work_day.sql
+++ SQL/Task_38/create table Calendar.work_day.sql
@@ -1,11 +1,13 @@
-﻿create schema Calendar
-
+﻿-- Создание схемы "Calendar"
+create schema Calendar
+-- Создание таблицы "Calendar.work_day"
 create table Calendar.work_day
 (
-	data Date not null,
+	work_date int identity not null,
+	date date not null,
 	work_day_type int not null,
-	work_week int not null
-	constraint PK__calendar primary key (data),
+	work_week int not null,
+	constraint PK__calendar primary key (work_date),
 	constraint FK__calendar__work_week foreign key (work_week) references Calendar.work_week,
 	constraint FK__calendar__work_day_type foreign key (work_day_type) references Calendar.work_day_type
 )
