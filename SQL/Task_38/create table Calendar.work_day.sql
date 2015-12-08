6e20fdf523f89d6be5cdc420e69ccb7f38520fb3
boync <sempai206@gmail.com>
2015-12-08 11:52:51 +0500

    ПЕ - 22б Яшин Создал схему запрос на создание схемы Calendar Создал запрос для создания таблицы Calendar.work_day

---
+++ SQL/Task_38/create table Calendar.work_day.sql
@@ -0,0 +1,12 @@
+﻿create schema Calendar
+
+create table Calendar.work_day
+(
+	data Date not null,
+	work_day_type int not null,
+	work_week int not null
+	constraint PK__calendar primary key (data),
+	constraint FK__calendar__work_week foreign key (work_week) references Calendar.work_week,
+	constraint FK__calendar__work_day_type foreign key (work_day_type) references Calendar.work_day_type
+)
+
