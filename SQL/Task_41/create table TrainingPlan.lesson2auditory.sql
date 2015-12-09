7863920b9c3faa1598311c04c3ae86dfd278b986
Томашов М.В <maxim.tomashov93@gmail.com>
2015-12-10 00:57:39 +1200

    ПЕ-22б Яковлев Я.О. Исправил: поле lesson2auditory должно быть первичным ключем а не lesson.

--- SQL/Task_41/create table TrainingPlan.lesson2auditory.sql
+++ SQL/Task_41/create table TrainingPlan.lesson2auditory.sql
@@ -4,9 +4,10 @@ go
 
 -- Создание таблицы TrainingPlan.lesson
 create table TrainingPlan.lesson2auditory(
-	lesson2auditory int identity,
+	lesson2auditory int identity ,
 	lesson int not null,
 	auditory int not null,  
-	constraint PK__lesson primary key (lesson),
-	constraint FK__lesson2auditory__auditory foreign key (auditory) references Auditory.auditory
+	constraint PK__lesson2auditory primary key (lesson2auditory),
+	constraint FK__lesson2auditory__auditory foreign key (auditory) references Auditory.auditory,
+	constraint FK__lesson2auditory__lesson foreign key (lesson) references TraningPlan.lesson
 )	
