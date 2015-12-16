42befc2c4eeb9e78a93385a4c4f835989be4efb7
OxanaPozdeeva <korombella@yandex.ru>
2015-12-17 00:57:19 +0500

    ПЕ-21б Поздеева О.В.: Создание сервиса комментариев.
    Добавила недобавленное :)

---
+++ IS.Mvc/IS.Mvc/Controllers/CommentController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using IS.Model.Item.Comment;
+using IS.Model.Service;
+using IS.Mvc.Models;
+
+namespace IS.Mvc.Controllers
+{
+	/// <summary>
+	/// Контролер для работы с данными по комментариям.
+	/// </summary>
+	public class CommentController : Controller
+	{
+		private CommentService _commentService;
+
+		public CommentController()
+		{
+			_commentService = new CommentService();
+		}
+
+		/// <summary>
+		/// Создает новый комментарий.
+		/// </summary>
+		/// <returns></returns>
+		[ValidateInput(false)]
+		public int SendMessage(CommentItem comment)
+		{
+			 _commentService.Create(comment);
+			return 0;
+		}
+	}
+}
