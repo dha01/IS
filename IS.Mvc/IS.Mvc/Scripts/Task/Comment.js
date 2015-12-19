50446b15f12b31a4c74b8308cd8d4a49712754ea
Уманов А.А <dha01@yandex.ru>
2015-12-19 23:27:32 +0500

    Уманов А.А.: исправлена пара косяков

--- IS.Mvc/IS.Mvc/Scripts/Task/Comment.js
+++ IS.Mvc/IS.Mvc/Scripts/Task/Comment.js
@@ -19,38 +19,38 @@ $(function () {
 	SetEvents();
 
 	$("#send").click(function () {
-       var message = $("#message").val();
-       var author = $("#author").val();
-       var task = $("#task").val();
-       var name = $("#name").val();
-       var d = new Date();
-       var day = d.getDate();
-       var mon = d.getMonth();
-       var year = d.getFullYear();
-       var hour = d.getHours();
-       var min = d.getMinutes();
-       var sec = d.getSeconds();
-       var date = day + "." + mon + "." + year + " " + hour + ":" + min + ":" + sec;
+		var message = $("#message").val();
+		var author = $("#author").val();
+		var task = $("#task").val();
+		var name = $("#name").val();
+		var d = new Date();
+		var day = d.getDate();
+		var mon = d.getMonth() + 1;
+		var year = d.getFullYear();
+		var hour = d.getHours();
+		var min = d.getMinutes();
+		var sec = d.getSeconds();
+		var date = day + "." + mon + "." + year + " " + hour + ":" + min + ":" + sec;
 
-       $.ajax({
-         type: "POST",
-         url: "/Comment/SendMessage",
-         data: {"PersonId": author,"Text": message, "TaskId": task, "AddDate":date},
-         cache: false,
-         success: function(response){
-             if(response.success === true){
-                $("#message").val("");
-             }
-             $("#commentBlock").append(
+		$.ajax({
+		type: "POST",
+		url: "/Comment/SendMessage",
+		data: {"PersonId": author,"Text": message, "TaskId": task, "AddDate":date},
+		cache: false,
+		success: function(response){
+			if(response.success === true){
+				$("#message").val("");
+			}
+			$("#commentBlock").append(
 				"<div style='border:2px solid lightgrey; width: 90%;  margin: 20px auto; padding: 0px 20px; white-space: pre-wrap;'>"
 				+ "<input type='hidden' class='input_comment_id' value='" + response.id +"'/>"
 				+ "<p>Автор: " + "<strong>" + name + "</strong></p><p>" + message + "</p>"
 				+ "<p>Добавлен: <strong>" + date + "</strong></p>"
 				+ (response.is_deleter === true ? "<button class='btm_delete'>Удалить</button>" : "" )
 				+ "<div>");
-	        SetEvents();
+			SetEvents();
 		}
 		});
-        return false;                                                         
-   });
+		return false;
+	});
 });
