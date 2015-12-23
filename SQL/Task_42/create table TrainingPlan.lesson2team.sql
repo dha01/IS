f17058010a46f3f9fb65d3c12fe37c1cb5a10b6e
cagey88 <chukreev8@gmail.com>
2015-12-24 00:22:51 +0500

    ПЕ-22б Чукреев Н.П.: Запрос на создание таблицы TrainingPlan.lesson2team

---
+++ SQL/Task_42/create table TrainingPlan.lesson2team.sql
@@ -0,0 +1,13 @@
+﻿-- Создание схемы TrainingPlan
+create schema TrainingPlan
+go
+
+-- Создание таблицы TrainingPlan.lesson2team
+create table TrainingPlan.lesson2team(	
+	lesson2team int identity,
+	lesson int not null,
+	team int not null,
+	constraint PK__lesson2team primary key (lesson2team),
+	constraint FK__lesson2team__team foreign key (team) references Team.team,
+	constraint FK__lesson2team__lesson foreign key (lesson) references TraningPlan.lesson
+)
