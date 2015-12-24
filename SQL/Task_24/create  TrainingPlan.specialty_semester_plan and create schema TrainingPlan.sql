e37d2b8048d57086a4f3cd9ac47494cbfcc4ad50
tasik73 <tasik73@yandex.ru>
2015-12-24 15:11:19 +0500

    ПЕ-21б Гросс АА: Создала запрос для создания таблицы TrainingPlan.specialty_semester_plan, и запрос на создание схемы TrainingPlan.

---
+++ SQL/Task_24/create  TrainingPlan.specialty_semester_plan and create schema TrainingPlan.sql
@@ -0,0 +1,33 @@
+﻿-- Создание схемы TrainingPlan
+create schema TrainingPlan
+
+-- Создание таблицы TrainingPlan.specialty_semester_plan
+create table TrainingPlan.specialty_semester_plan(
+	specialty_semester_plan int identity (1,1) not null,
+	discipline int not null,
+	specialty_detail int not null,
+	semester int not null,
+	lesson_type int not null,
+	plan_time int not null,
+	constraint PK__specialty_semester_plan primary key (specialty_semester_plan),
+	constraint FK__specialty_semester_plan__discipline foreign key (discipline) references Discipline.discipline,
+	constraint FK__specialty_semester_plan__specialty_detail foreign key (specialty_detail) references Specialty.specialty_detail,
+	constraint FK__specialty_semester_plan__semester foreign key (semester) references Calendar.semester,
+	constraint FK__specialty_semester_plan__lesson_type foreign key (lesson_type) references TrainingPlan.lesson_type,
+)
+
+-- Добавление записи в таблицу TrainingPlan.specialty_semester_plan
+insert into TrainingPlan.specialty_semester_plan (
+	discipline,
+	specialty_detail,
+	semester,
+	lesson_type,
+	plan_time
+	)
+	values
+	(1,
+	1,
+	2,
+	3,
+	144
+    )
